a673db354d5226be333e579652f07f5fb9652d96
Anderson Cesar <andersonlscesar@gmail.com>
2022-07-12 15:22:28 -0300

    um pouco sobre parâmetros e funções

--- chapter_1/parametros_funcoes/parametros.php
+++ chapter_1/parametros_funcoes/parametros.php
@@ -1,4 +1,32 @@
 <?php
 /**
  * Especificando o tipo de dados que recebemos no parâmetro da função
+ * @param float 
+ * @param float
+ * @return float
  */
+
+ function calcula_imc(float $peso, float $altura) {
+    return $peso / ($altura * $altura);
+ }
+
+//  var_dump(calcula_imc(86, 1.85)); 
+
+/**
+ * Este tipo de conversão pode não conseguir converter todos os valores que possam ser passados em $peso ou em $altura
+ */
+
+
+ /**
+  * Especificando  que receberemos float no parâmetro da função e forçando o retorno a ser do tipo int
+ * @param float
+ * @param float
+ * @return int
+ * 
+  */
+
+  function calcula_imc1(float $peso, float $altura):int {
+    return $peso / ($altura * $altura);
+  }
+
+//   var_dump(calcula_imc1(86, 1.85));
