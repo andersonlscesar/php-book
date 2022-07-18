f1835331c43d56602aab07c8705699fa8c6c0580
Anderson Cesar <andersonlscesar@gmail.com>
2022-07-18 09:11:50 -0300

    Agragação e polimorfismo

---
+++ chapter_2/classes/ContaPoupanca.php
@@ -0,0 +1,12 @@
+<?php
+class ContaPoupanca extends Conta {
+    public function retirar($quantia) {
+        if($this->saldo >= $quantia) {
+            $this->saldo -= $quantia;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ chapter_2/relacinamento/agregacao.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__.'/../classes/Cesta.php';
+require_once __DIR__.'/../classes/Produto.php';
+
+// Criação  da cesta
+$c1 = new Cesta;
+
+//Agregação dos produtos
+
+$c1->addItem($p1 = new Produto('Chocolate', 10, 5));
+$c1->addItem($p1 = new Produto('Café', 100, 7));
+$c1->addItem($p1 = new Produto('Mostarda', 50, 3));
+
+// $c1->addItem('Teste');
+
+foreach($c1->getItens() as $item) {
+    print 'Item: '.$item->getDescricao().'<br>'.PHP_EOL;
+}
